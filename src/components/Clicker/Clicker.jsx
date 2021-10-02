ac133939497f0d85cedb37ce44066b4d4ff76c8b
Anastasia <18anastasiasneshko@gmail.com>
2021-10-02 13:23:05 +0300

    Add Clicker functional

--- src/components/Clicker/Clicker.jsx
+++ src/components/Clicker/Clicker.jsx
@@ -1,12 +1,38 @@
 import { Component } from 'react';
 
-class Clicker extends Component {
-  render() {
+class Clicker extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      clickAmount: 0
+    };
+  }
+
+  nextStepClick = () => {
+    const{clickAmount} = this.state;
+    this.setState({
+      clickAmount: clickAmount + 1
+    });
+  }
+
+  prevStepClick = () => {
+    const{clickAmount} = this.state;
+    this.setState({
+      clickAmount: clickAmount - 1
+    });
+  }
+
+  render(){
+    const {clickAmount} = this.state;
+
+    const prevBtn = <button className="btn" onClick={this.prevStepClick}>Отнять</button>
+
     return (
-      <div>
-        
-      </div>
-    );
+    <div>
+      <h1 className="clickerTest">Кликнул {clickAmount} раз</h1>
+      <button className="btn" onClick={this.nextStepClick}>Добавить</button>
+      {clickAmount > 0 ? prevBtn : false}
+    </div>)
   }
 }
 
